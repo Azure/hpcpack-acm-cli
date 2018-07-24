91541e7d60c1e745efa6483062ef6d1930abcaaf
Robert Zhang <louirobert@gmail.com>
2018-07-24 17:14:10 +0800

    Workaround the issue that Python package installation can't copy files to user home.

--- hpc_acm_cli/command.py
+++ hpc_acm_cli/command.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import sys
 import os.path
+import shutil
 import configparser
 import argparse
 from hpc_acm_cli.parser_builder import ParserBuilder
@@ -11,7 +12,9 @@ from hpc_acm.api_client import ApiClient
 from hpc_acm.rest import ApiException
 
 class Command:
-    config_path = os.path.join('~', '.hpc_acm_cli_config')
+    config_file_name = '.hpc_acm_cli_config'
+    config_dir = os.path.expanduser('~')
+    config_path = os.path.join(config_dir, config_file_name)
 
     def __init__(self, args):
         config = Configuration()
@@ -52,14 +55,24 @@ class Command:
             }
         return spec
 
+    # The reason to have this method is explained in setup.py of the containing package.
+    # Simply put, the Python package installation can't copy the default config to user home.
+    @classmethod
+    def make_sure_default_config(cls):
+        if not os.path.exists(cls.config_path):
+            path = sys.modules['hpc_acm_cli'].__file__
+            dist_path = os.path.dirname(os.path.dirname(path))
+            shutil.copyfile(os.path.join(dist_path, cls.config_file_name), cls.config_path)
+
     @classmethod
     def run(cls):
+        cls.make_sure_default_config()
         spec = cls.build_spec()
         parser = ParserBuilder.build(spec)
         args = parser.parse_args()
         if args.config:
             config = configparser.ConfigParser()
-            if config.read(os.path.expanduser(args.config)):
+            if config.read(args.config):
                 args = Arguments(args, config)
             elif args.config != cls.config_path:
                 print('Config file %s is not found!' % args.config, file=sys.stderr)
